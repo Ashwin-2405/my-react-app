cf690d85b85eb77778d5373d3ed5196831692c87
Ashwin <ashwinsp2405@gmail.com>
2025-07-04 22:35:40 +0530

    Day 8: First React app with Vite

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Hello, React World 👋</h1>
+      <p>This is your first React component!</p>
+    </div>
+  );
+}
+
+export default App;
